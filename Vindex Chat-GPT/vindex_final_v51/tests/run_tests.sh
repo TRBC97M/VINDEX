36bce896ebc6625e2a1ae382de427b755e94c1f4
TRBC97M <trbc97m@protonmail.com>
2026-08-22 05:36:20 +0200

    VINDEX 0.53: vocationes nullas in regressione custodi

--- Vindex Chat-GPT/vindex_final_v51/tests/run_tests.sh
+++ Vindex Chat-GPT/vindex_final_v51/tests/run_tests.sh
@@ -142,7 +142,8 @@ exsequere_casum "importa" "tests/casus/importa.vindex" "49"
 exsequere_casum "structura-acus" "tests/casus/structura_acus.vindex" $'150\n99'
 exsequere_casum "recursio" "tests/casus/recursio.vindex" "720"
 exsequere_casum "argumenta" "tests/casus/argumenta.vindex" $'2\n90' "Zeta"
-exsequere_casum "vxnat-partem" "tests/casus/vxnat_partem.vindex" $'83\n65\n76\n86\n69'
+exsequere_casum "vxnat-partem" "tests/casus/vxnat_partem.vindex" $'83\n65\n76\n86'
+exsequere_casum "vocationes-nullae" "tests/casus/vocationes_nullae.vindex" "7168"
 
 respice_reiectionem "erratum-principalis" "tests/casus/erratum_principalis.vindex" "FUNCTIO PRINCIPALIS deest"
 respice_reiectionem "erratum-functio" "tests/casus/erratum_functio.vindex" "functio 'FUNCTIO_IGNOTA' non definita"
