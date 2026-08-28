96ec0c4e05beca7719c08faf4e62fc8d28176572
TRBC97M <trbc97m@protonmail.com>
2026-08-28 17:20:42 +0200

    P16-XI: custodiam Bureau ad materiales JL-UX migra

--- Vindex Chat-GPT/vindex_final_v51/instrumenta/proba_bureau_sylviae_iii.py
+++ Vindex Chat-GPT/vindex_final_v51/instrumenta/proba_bureau_sylviae_iii.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""P16-IX: bureau, launch, clausura et relaunch cum taskbar composita sub UEFI/QEMU comprobantur."""
+"""P16-XI: bureau, hover, launch, clausura et relaunch cum taskbar IX sub UEFI/QEMU comprobantur."""
 from __future__ import annotations
 
 import importlib.util
@@ -23,6 +23,26 @@ def lumen(color: tuple[int, int, int]) -> int:
     return color[0] + color[1] + color[2]
 
 
+def differentiae_rect(p1: bytes, p2: bytes, w: int, x0: int, y0: int, x1: int, y1: int) -> int:
+    n = 0
+    for y in range(y0, y1):
+        for x in range(x0, x1):
+            i = (y * w + x) * 3
+            if p1[i:i+3] != p2[i:i+3]:
+                n += 1
+    return n
+
+
+def copia_coloris(pix: bytes, w: int, x0: int, y0: int, x1: int, y1: int, color: tuple[int, int, int]) -> int:
+    n = 0
+    for y in range(y0, y1):
+        for x in range(x0, x1):
+            i = (y * w + x) * 3
+            if tuple(pix[i:i+3]) == color:
+                n += 1
+    return n
+
+
 def principale() -> int:
     if len(sys.argv) != 5:
         print('USUS: proba_bureau_sylviae_iii.py MONITOR QMP EXITUS MORA', file=sys.stderr)
@@ -44,9 +64,8 @@ def principale() -> int:
         aux.lege_usque(monitor, b'(qemu) ', 2.0)
         time.sleep(mora)
 
-        nox_bureau = (28, 31, 32)
-        papyrus = (215, 205, 185)
-        bronzeum = (185, 138, 82)
+        bronzeum_viii = (185, 138, 82)
+        aqua_xi = (189, 239, 242)
 
         ante = out / 'bureau-ante.ppm'
         hover_p = out / 'bureau-hover-programmata.ppm'
@@ -71,38 +90,36 @@ def principale() -> int:
             print(f'DEFECIT: taskbar IX vacua gradientiam amisit: {vac_top}->{vac_bottom}', file=sys.stderr)
             return 6
 
-        # Bureau P16-VII manet: tesserae et hover historici non moventur.
-        if aux.pixel(pix_ante, w, 20, 74) != nox_bureau or aux.pixel(pix_ante, w, 20, 178) != nox_bureau:
-            print('DEFECIT: tesserae bureau nocturnae initiales desunt', file=sys.stderr)
-            return 7
-
+        # P16-XI: hitbox manet, sed tessera nunc materia Graphica VIII est.
         pos_p = aux.move_ad(monitor, out, 'bureau-programmata', 70, 110, w, h)
         aux.captura(monitor, hover_p)
         _, _, pix_hover_p = aux.ppm(hover_p)
-        if aux.pixel(pix_hover_p, w, 20, 74) != papyrus:
-            print(f'DEFECIT: hover PROGRAMMATA papyraceus non detectus: {aux.pixel(pix_hover_p,w,20,74)} cursor={pos_p}', file=sys.stderr)
-            return 8
+        mut_hover = differentiae_rect(pix_ante, pix_hover_p, w, 18, 72, 126, 160)
+        aqua_ante = copia_coloris(pix_ante, w, 18, 72, 126, 160, aqua_xi)
+        aqua_hover = copia_coloris(pix_hover_p, w, 18, 72, 126, 160, aqua_xi)
+        if mut_hover < 80 or aqua_hover <= aqua_ante + 20:
+            print(f'DEFECIT: hover PROGRAMMATA XI non mutat materiam satis: mut={mut_hover} aqua={aqua_ante}->{aqua_hover} cursor={pos_p}', file=sys.stderr)
+            return 7
 
         aux.click(monitor)
         aux.captura(monitor, apertum_p)
         _, _, pix_p = aux.ppm(apertum_p)
-        if aux.pixel(pix_p, w, 300, 56) != bronzeum:
+        if aux.pixel(pix_p, w, 300, 56) != bronzeum_viii:
             print(f'DEFECIT: PROGRAMMATA non aperta/focalizata: {aux.pixel(pix_p,w,300,56)}', file=sys.stderr)
-            return 9
+            return 8
 
         prog_top = aux.pixel(pix_p, w, 130, taskbar_top + 8)
         prog_bottom = aux.pixel(pix_p, w, 130, taskbar_top + 31)
         if prog_top == prog_bottom or lumen(prog_top) <= lumen(prog_bottom):
             print(f'DEFECIT: PROGRAMMATA in taskbar IX non est graduata: {prog_top}->{prog_bottom}', file=sys.stderr)
-            return 10
+            return 9
         if (prog_top, prog_bottom) == (vac_top, vac_bottom):
             print('DEFECIT: PROGRAMMATA in taskbar non apparuit', file=sys.stderr)
-            return 11
-        if aux.pixel(pix_p, w, 130, taskbar_top + 6) != bronzeum:
+            return 10
+        if aux.pixel(pix_p, w, 130, taskbar_top + 6) != bronzeum_viii:
             print(f'DEFECIT: PROGRAMMATA taskbar activa limen aeneum amisit: {aux.pixel(pix_p,w,130,taskbar_top+6)}', file=sys.stderr)
-            return 12
+            return 11
 
-        # Bulla clausurae PROGRAMMATA: geometria canonica circa (798,74).
         pos_close = aux.move_ad(monitor, out, 'bureau-clausura-programmata', 798, 74, w, h)
         aux.click(monitor)
         aux.captura(monitor, clausum_p)
@@ -111,38 +128,39 @@ def principale() -> int:
         clausum_bottom = aux.pixel(pix_clausum, w, 130, taskbar_top + 31)
         if (clausum_top, clausum_bottom) != (vac_top, vac_bottom):
             print(f'DEFECIT: PROGRAMMATA post clausuram in taskbar manet: {clausum_top}/{clausum_bottom}', file=sys.stderr)
-            return 13
-        if aux.pixel(pix_clausum, w, 300, 56) == bronzeum:
+            return 12
+        if aux.pixel(pix_clausum, w, 300, 56) == bronzeum_viii:
             print('DEFECIT: PROGRAMMATA post clausuram adhuc pingitur', file=sys.stderr)
-            return 14
+            return 13
 
         pos_t = aux.move_ad(monitor, out, 'bureau-tabula', 70, 214, w, h)
         aux.click(monitor)
         aux.captura(monitor, apertum_t)
         _, _, pix_t = aux.ppm(apertum_t)
-        if aux.pixel(pix_t, w, 700, 168) != bronzeum:
+        if aux.pixel(pix_t, w, 700, 168) != bronzeum_viii:
             print(f'DEFECIT: TABULA ex bureau non aperta/focalizata: {aux.pixel(pix_t,w,700,168)}', file=sys.stderr)
-            return 15
+            return 14
 
         tab_top = aux.pixel(pix_t, w, 130, taskbar_top + 8)
         tab_bottom = aux.pixel(pix_t, w, 130, taskbar_top + 31)
         if tab_top == tab_bottom or lumen(tab_top) <= lumen(tab_bottom):
             print(f'DEFECIT: TABULA in taskbar IX non est graduata: {tab_top}->{tab_bottom}', file=sys.stderr)
-            return 16
+            return 15
         if (tab_top, tab_bottom) == (vac_top, vac_bottom):
             print('DEFECIT: TABULA in taskbar activa non apparuit', file=sys.stderr)
-            return 17
-        if aux.pixel(pix_t, w, 130, taskbar_top + 6) != bronzeum:
+            return 16
+        if aux.pixel(pix_t, w, 130, taskbar_top + 6) != bronzeum_viii:
             print(f'DEFECIT: TABULA taskbar activa limen aeneum amisit: {aux.pixel(pix_t,w,130,taskbar_top+6)}', file=sys.stderr)
-            return 18
+            return 17
 
         mut_p = aux.differentiae(pix_ante, pix_p)
         mut_close = aux.differentiae(pix_p, pix_clausum)
         mut_t = aux.differentiae(pix_clausum, pix_t)
-        print(f'BUREAU-IX: cursor_init={init_pos} programmata={pos_p} clausura={pos_close} tabula={pos_t}')
-        print(f'BUREAU-IX: taskbar_vacua={vac_top}->{vac_bottom} programmata={prog_top}->{prog_bottom} tabula={tab_top}->{tab_bottom}')
-        print(f'BUREAU-IX: launch_programmata_pixeli={mut_p} clausura_pixeli={mut_close} launch_tabula_pixeli={mut_t}')
-        print('RECTE: P16-IX bureau applicationes aperit, claudit et taskbar compositam recte renovat.')
+        print(f'BUREAU-XI: cursor_init={init_pos} programmata={pos_p} clausura={pos_close} tabula={pos_t}')
+        print(f'BUREAU-XI: hover_pixeli={mut_hover} aqua={aqua_ante}->{aqua_hover}')
+        print(f'BUREAU-XI: taskbar_vacua={vac_top}->{vac_bottom} programmata={prog_top}->{prog_bottom} tabula={tab_top}->{tab_bottom}')
+        print(f'BUREAU-XI: launch_programmata_pixeli={mut_p} clausura_pixeli={mut_close} launch_tabula_pixeli={mut_t}')
+        print('RECTE: P16-XI Bureau Lucidum hover/launch/clausura/relaunch et taskbar IX servantur.')
         return 0
     finally:
         try:
